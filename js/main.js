05b8313e0f76a34b095dfb87e848becde667a483
McKenna Poulos <mckennapoulos@McKennas-MacBook-Pro.local>
2019-06-18 19:30:32 -0400

    new files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#QAButton').on('click', function() {
+  $('#QAButton').css('opacity', 1);
+});
+$('#UXButton').on('click', function() {
+  $('#QAButton').css('opacity', 1);
+});
+$('#EngineerButton').on('click', function() {
+  $('#QAButton').css('opacity', 1);
+});
