90413b27e440d449d38fcee3b121949e9a24de67
Matthew Swenson <imprivatahackathon@gmail.com>
2019-06-18 17:01:03 -0400

    added sign in functionality

--- scripts/main2.js
+++ scripts/main2.js
@@ -16,30 +16,34 @@ if (!firebase.apps.length) {
 
 $('#view-projects').on('click', function() {
   var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider);
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    currentUID = user.uid
+    console.log("success");
+    writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+    window.open("/../projectListPage.html");
+    // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode)
+      console.log(errorMessage)
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      currentUID = null;
+      console.log(email)
+      // ...
+    });
 });
 
-firebase.auth().onAuthStateChanged(onAuthStateChanged);
 var currentUID;
 
-function onAuthStateChanged(user) {
-  // We ignore token refresh events.
-  if (user && currentUID === user.uid) {
-    return;
-  }
-
-  if (user) {
-    currentUID = user.uid;
-    window.open("/projectListPage.html")
-    writeUserData(user.uid, user.displayName, user.email, user.photoURL);
-    //start to get the database info
-  } else {
-    // Set currentUID to null.
-    currentUID = null;
-    // Display the splash page where you can sign-in.
-    //window.open("/index.html")
-  }
-}
 
 function writeUserData(userId, name, email, imageUrl) {
   firebase.database().ref('users/' + userId).set({
